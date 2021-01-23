1d84f1df94dd525dbb9edb11ec9742b1239d9566
Christian Zoppi <me@christianzoppi.com>
2021-01-23 14:56:53 +0100

    Init

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+const Storyblok11Ty = require("storyblok-11ty");
+const sbto11ty = new Storyblok11Ty.plugin();
+require('dotenv').config();
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(sbto11ty);
+};
