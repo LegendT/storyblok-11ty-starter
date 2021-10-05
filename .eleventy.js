7f39476c2645ea523554b3f1b80c8c95087fce01
Anthony George <ageorge@nomensa.com>
2021-10-05 16:32:13 +0100

    components added and njk files added (to try and get these working later)

--- .eleventy.js
+++ .eleventy.js
@@ -6,4 +6,18 @@ const sbto11ty = new Storyblok11Ty.plugin()
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({ src: './' })
 	eleventyConfig.addPlugin(sbto11ty)
+
+	// Base Config
+	return {
+		// dir: {
+		// 	input: 'src',
+		// 	output: 'dist',
+		// 	includes: '_includes',
+		// 	layouts: '_layouts',
+		// 	data: '_data',
+		// },
+		templateFormats: ['njk', 'liquid', 'md', 'css', 'png', 'jpg', 'svg'],
+		htmlTemplateEngine: 'njk',
+		markdownTemplateEngine: 'njk',
+	}
 }
