c385a300376d048560e982631bdf837e426c805c
Anthony George <ageorge@nomensa.com>
2021-10-05 17:08:43 +0100

    WIP - adding scripts and layouts from wcag-reporter

--- .eleventy.js
+++ .eleventy.js
@@ -3,19 +3,28 @@ require('dotenv').config()
 const Storyblok11Ty = require('storyblok-11ty')
 const sbto11ty = new Storyblok11Ty.plugin()
 
+const fs = require('fs')
+const path = require('path')
+const markdownShortcode = require('eleventy-plugin-markdown-shortcode')
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const scTable = require('./_utils/scTable.js')
+const sampleImage = require('./_utils/sampleImage.js')
+const scUri = require('./_utils/scUri.js')
+const scName = require('./_utils/scName.js')
+const slugify = require('./_utils/slugify.js')
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({ src: './' })
 	eleventyConfig.addPlugin(sbto11ty)
 
 	// Base Config
 	return {
-		// dir: {
-		// 	input: 'src',
-		// 	output: 'dist',
-		// 	includes: '_includes',
-		// 	layouts: '_layouts',
-		// 	data: '_data',
-		// },
+		dir: {
+			output: '_site',
+			includes: '_includes',
+			layouts: '_layouts',
+			data: '_data',
+		},
 		templateFormats: ['njk', 'liquid', 'md', 'css', 'png', 'jpg', 'svg'],
 		htmlTemplateEngine: 'njk',
 		markdownTemplateEngine: 'njk',
